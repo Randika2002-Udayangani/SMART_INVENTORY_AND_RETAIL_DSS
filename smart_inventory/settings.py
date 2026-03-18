d2e372993614627b4982943f9899aa90360af40e
lavanya-2342 <lavanyawanigasekara33@gmail.com>
2026-03-18 11:02:35 +0530

    Week3: Add dashboard app with home.html, views and urls

--- smart_inventory/settings.py
+++ smart_inventory/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'purchases',
     'sales',
     'users',
+    'dashboard', 
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'smart_inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
