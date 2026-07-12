6f9669730b5c2720aacb80602e0af81484baa945
lavanya-2342 <lavanyawanigasekara33@gmail.com>
2026-07-12 19:02:57 +0530

    Week6: Tasks complete - Loss Analysis, Lifecycle, sidebar fixes, sessionStorage, Health Score filter

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.dashboard_home, name='dashboard_home'),
     path('login/', views.dashboard_login, name='dashboard_login'),
     path('sales-report/', views.sales_report, name='sales_report'),
+    path('loss-analysis/', views.loss_analysis, name='loss_analysis'),
+    path('lifecycle/', views.lifecycle, name='lifecycle'),
 ]
