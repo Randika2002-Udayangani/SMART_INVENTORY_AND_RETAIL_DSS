a9f5ba7464f560157a599003ad32805e001e5be5
chalani073 <chalanithishara@gmail.com>
2026-09-20 13:20:06 +0530

    Show expiry date and real stock count in product modal

--- products/views.py
+++ products/views.py
@@ -153,6 +153,8 @@ class ProductAvailabilityView(APIView):
         return Response({
             'status': availability_status,
             'can_order': can_order,
+            'stock': int(current_stock),
+
         })
 
 
